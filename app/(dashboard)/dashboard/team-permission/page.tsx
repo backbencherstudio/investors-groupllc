be1be2f8b6b69a3211f9bed5fd842ef087014801
hrid1 <hridoycse1@gmail.com>
2025-06-28 17:04:09 +0600

    Setup team permission

---
+++ app/(dashboard)/dashboard/team-permission/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <h3 className='text-lg font-medium mb-6'>Team Permission</h3>
+
+        
+        <section className='p-6 rounded-lg bg-white'>
+            <h2 className='text-2xl text-[#170A00] font-semibold'>Team Permission</h2>
+            {/* table */}
+        </section>
+    </div>
+  )
+}
