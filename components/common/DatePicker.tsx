27d7aeb50f2cdeefd812d86a1b3b088ee1c23164
hrid1 <hridoycse1@gmail.com>
2025-07-11 16:28:44 +0600

    update admin subscription and calender

--- components/common/DatePicker.tsx
+++ components/common/DatePicker.tsx
@@ -1,34 +1,62 @@
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { CalendarIcon } from "lucide-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { ChevronDownIcon } from "lucide-react";
+import { Label } from "@/components/ui/label";
 import { useState } from "react";
 
-export default function DatePicker({ value, onChange }: { value: Date | undefined, onChange: (date: Date | undefined) => void }) {
+export default function DatePicker({
+  value,
+  onChange,
+  label = "",
+  id = "date",
+}: {
+  value: Date | undefined;
+  onChange: (date: Date | undefined) => void;
+  label?: string;
+  id?: string;
+}) {
   const [open, setOpen] = useState(false);
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button variant="outline" className="w-full md:w-[160px] justify-start text-left font-normal">
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {value ? value.toDateString() : "Select date"}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
-        <Calendar 
-          selected={value} 
-          onSelect={(date) => { 
-            if (date instanceof Date) {
+    <div className="flex flex-col gap-3">
+      {label && (
+        <Label htmlFor={id} className="px-1">
+          {label}
+        </Label>
+      )}
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            id={id}
+            className={`justify-between font-normal ${
+              !value && "text-gray-600"
+            }`}
+          >
+            {value ? value.toLocaleDateString() : "Select date"}
+            <ChevronDownIcon />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent
+          className="w-auto overflow-hidden p-0 shadow mr-6"
+          align="start"
+        >
+          <Calendar
+            mode="single"
+            selected={value}
+            captionLayout="dropdown"
+            onSelect={(date) => {
               onChange(date);
-            } else {
-              onChange(undefined);
-            }
-            setOpen(false); 
-          }} 
-          initialFocus 
-        />
-      </PopoverContent>
-    </Popover>
+              setOpen(false);
+            }}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
   );
 }
