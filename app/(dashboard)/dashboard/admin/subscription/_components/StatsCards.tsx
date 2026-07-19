3bb5cc42a3294afd258a66c57238a703ec0d1811
mahmudul-hasan01 <mahamudul2018@gmail.com>
2026-07-19 15:26:49 +0600

    Change status data.

--- app/(dashboard)/dashboard/admin/subscription/_components/StatsCards.tsx
+++ app/(dashboard)/dashboard/admin/subscription/_components/StatsCards.tsx
@@ -1,44 +1,19 @@
 "use client";
-import React from "react";
-import { useGetSubscriptionStatsQuery } from "@/redux/features/subscription/SubscriptionApi";
-import Diamond from "@/components/icons/subscription/Diamond";
-import Monthly from "@/components/icons/subscription/Monthly";
-import People from "@/components/icons/subscription/People";
-import Revinew from "@/components/icons/subscription/Revinew";
+
 import { StatsCardsSkeleton } from "@/components/common/Loader";
 
-export default function StatsCards() {
-  const { data: stats, isLoading, error } = useGetSubscriptionStatsQuery();
-
-  // Prepare card data from real API response
-  const cardData = [
-    {
-      icon: People,
-      value: stats?.totalSubs ?? 0,
-      label: "Total Subscribers",
-    },
-    {
-      icon: Monthly,
-      value: stats?.monthlyPlan ?? 0,
-      label: "Monthly Plan",
-    },
-    {
-      icon: Diamond,
-      value: stats?.yearlyPlan ?? 0,
-      label: "Yearly Plan",
-    },
-    {
-      icon: Revinew,
-      value: `$${stats?.revenue?.totalPlanAmount ?? 0}`,
-      label: "Revenue",
-    },
-  ];
-
+export default function StatsCards({
+  cardData,
+  isLoading,
+  error,
+}: {
+  cardData?: any;
+  isLoading?: any;
+  error?: any;
+}) {
   // Loading state
   if (isLoading) {
-    return (
-     <StatsCardsSkeleton />
-    );
+    return <StatsCardsSkeleton />;
   }
 
   // Error state
@@ -52,13 +27,13 @@ export default function StatsCards() {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
-      {cardData.map((item, idx) => (
+      {cardData?.map((item: any, idx: number) => (
         <div
           key={idx}
           className="flex items-center bg-white rounded-xl shadow p-4 lg:p-6 min-w-[180px]"
         >
-         {/* Icon box */}
-         <div className="flex items-center justify-center w-16 h-16 rounded-lg bg-orange-50 mr-4 ">
+          {/* Icon box */}
+          <div className="flex items-center justify-center w-16 h-16 rounded-lg bg-orange-50 mr-4 ">
             {item.icon && <item.icon />}
           </div>
           {/* Number and label */}
